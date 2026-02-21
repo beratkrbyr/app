eed84480a1967bf6627441764fc9039215b43c5e
Berat Karabayir <beratkrbyr@gmail.com>
2026-02-21 13:36:17 +0000

    auto-commit for 52151322-833b-46ed-889b-9d6aeafea38d

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -8,7 +8,7 @@ import requests
 import os
 import time
 
-BASE_URL = os.environ.get('REACT_APP_BACKEND_URL', 'https://cleaning-app-test.preview.emergentagent.com')
+BASE_URL = os.environ.get('REACT_APP_BACKEND_URL', 'https://cleaning-booking-app.preview.emergentagent.com')
 TEST_PHONE = f"TEST_{int(time.time())}"  # Unique phone for each test run
 
 class TestHealthAndInit:
